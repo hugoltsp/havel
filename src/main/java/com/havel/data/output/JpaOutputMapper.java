0e0833de979ce64b94f8b934ef9419513165b679
Hugo <hugo-pires@outlook.com>
2016-05-31 00:20:18 -0300

    JpaOutputMapper + test

--- src/main/java/com/havel/data/output/JpaOutputMapper.java
+++ src/main/java/com/havel/data/output/JpaOutputMapper.java
@@ -2,7 +2,6 @@ package com.havel.data.output;
 
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 import javax.persistence.Column;
@@ -27,17 +26,18 @@ public class JpaOutputMapper<O> implements OutputMapper<O> {
 	}
 
 	private O resultSetToJpaEntity(ResultSet rs) throws InstantiationException, IllegalAccessException, SQLException {
-		ResultSetMetaData metaData = rs.getMetaData();
-
 		O newInstance = this.entityType.newInstance();
-
 		Field[] fields = this.entityType.getDeclaredFields();
 
 		for (int i = 0; i < fields.length; i++) {
 			Field field = fields[i];
-			if(field.isAnnotationPresent(Column.class)){
+			if (field.isAnnotationPresent(Column.class)) {
 				Column column = field.getAnnotation(Column.class);
 				String columnName = column.name();
+				if (!field.isAccessible()) {
+					field.setAccessible(true);
+				}
+				field.set(newInstance, rs.getObject(columnName, field.getType()));
 			}
 		}
 
--- src/test/java/com/havel/tests/OutputTests.java
+++ src/test/java/com/havel/tests/OutputTests.java
@@ -1,22 +1,25 @@
 package com.havel.tests;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.havel.builder.Batch;
 import com.havel.data.input.SqlInput;
 import com.havel.data.output.JpaOutputMapper;
-import com.havel.tests.util.User;
 import com.havel.tests.util.UserEntity;
 
 public class OutputTests extends HavelTests {
 
 	@Test
 	public void jpaOutputMapperTest() throws Exception {
-		Batch.<UserEntity> bulkSelect().connection(connection).input(new SqlInput("select * from user where id=1"))
-				.outputMapper(new JpaOutputMapper(UserEntity.class)).select().findAny().ifPresent(System.out::println);
+		UserEntity userEntity = Batch.<UserEntity> bulkSelect().connection(connection)
+				.input(new SqlInput("select * from user where id=1"))
+				.outputMapper(new JpaOutputMapper<UserEntity>(UserEntity.class)).select().findAny().get();
 
+		Assert.assertTrue(userEntity != null);
+		Assert.assertTrue(userEntity.getId() != null);
+		Assert.assertTrue(userEntity.getEmail() != null);
+		Assert.assertTrue(userEntity.getName() != null);
 	}
 
 }
